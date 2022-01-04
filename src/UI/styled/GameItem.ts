a2eeceeaab1f96a88882f70c1a22e5e82786dd70
Subbotin Valery <subbotin.v@uspect.ru>
2022-01-04 05:20:29 +0300

    feat: upgrade styles

--- src/UI/styled/GameItem.ts
+++ src/UI/styled/GameItem.ts
@@ -2,13 +2,26 @@ import { styled } from "./Theme";
 import { transition } from "../../constants/styleVariables";
 
 export const GameItem = styled.div`
-  cursor: pointer;
+  position: relative;
   padding: 2rem;
-  border: ${(props) => `0.1rem solid ${props.theme.contrast}`};
-  border-radius: 2.5rem;
   transition: ${transition};
+  background-color: ${(props) => props.theme.main_lighten};
   &:hover {
-    box-shadow: 0.5rem 0.5rem 1rem 0 ${(props) => props.theme.main_darken};
+    box-shadow: 0 0 1rem 0 ${(props) => props.theme.main_darken};
+  }
+  &::before {
+    position: absolute;
+    content: "";
+    display: block;
+    background-color: ${(props) => props.theme.contrast};
+    height: 0.1rem;
+    width: 0;
+    transition: ${transition};
+    bottom: 0;
+    left: 0;
+  }
+  &:hover::before {
+    width: 100%;
   }
   & span {
     font-size: 3rem;
